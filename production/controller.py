c16d638be54f4c49d2c1796debef0bb34b18f9b2
Gabriel <teigne.gabriel@gmail.com>
2024-02-13 17:22:07 +0100

    Merge camera and lines processing function

--- production/controller.py
+++ production/controller.py
@@ -3,13 +3,16 @@ import sys
 directory = path.Path(__file__).abspath()
 sys.path.append(directory.parent.parent)
 from camera.cameraSettings import Camera
+from linesProcessing.main import LinesProcess
 
 class Controller:
     def __init__(self) -> None:
         self.camera = Camera()
+        self.linesProcess = LinesProcess(self.camera.mid)
 
     def start(self):
         self.camera.start()
         while self.camera.isRecording:
-            self.camera.detectLineInFrame(preview=False, saveOutput=False)
+            coords = self.camera.detectLineInFrame(preview=False, saveOutput=False)
+            pos = self.linesProcess.output(coords)
         self.camera.stop()
