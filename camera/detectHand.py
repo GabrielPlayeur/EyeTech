71ef0a854fa08fd2d02740b5307246dbe740d7ee
Landvick <patrick.julienne22@gmail.com>
2024-02-20 16:01:30 +0100

    2nd test détection de la main sérieusement fait

--- camera/detectHand.py
+++ camera/detectHand.py
@@ -1,16 +1,16 @@
 import cv2
+from cameraSettings import Camera
 
 hand_cascade = cv2.CascadeClassifier('palm.xml')
-cap = cv2.VideoCapture(0)
+cam = Camera()
 
 while True:
     # Lire une image de la webcam
-    _, frame = cap.read()
+    frame = cam.capture_array()
     # Détecter les mains dans l'image    
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #Converts image to grayscale
     hands = hand_cascade.detectMultiScale(gray, scaleFactor=1.6, minNeighbors=5) #All detected hands get put into an array
-    hands = hand_cascade.detectMultiScale( 1.1, 5)
-
+    print(len(hands))
 
 # Libérer la capture vidéo et fermer les fenêtres OpenCV
 cap.release()
