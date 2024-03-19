efa39b29bb6a445716be5f443dfebe8debff7405
Nathan <nathrivron@gmail.com>
2024-03-19 16:38:39 +0100

    docstring et try except lineprocessing

--- linesProcessing/lineProcess.py
+++ linesProcessing/lineProcess.py
@@ -8,6 +8,16 @@ class LinesProcess:
         self.delay = 0.5
 
     def calcul(self, lignes: dict) -> list:
+        """fonction permettant de calculer la position relative du coureur par rapport au centre du couloir
+        entrée : dict de listes (coordonnées)
+        sortie : liste de float (pourcentage de déviation par rapport au centre [ % gauche, % droite])"""
+        try:
+            lignes["right"][3]
+            lignes["left"][3]
+        except Exception as error:
+            print(error)
+            return [0,0]
+
         if lignes["right"][1] >= lignes["right"][3]:
             minXRight = lignes["right"][2]
         else:
@@ -24,12 +34,18 @@ class LinesProcess:
         return [abs(round((center-self.milieu)/(center-minXLeft+0.001),2)),0] #Left
 
     def updateQueue(self, value: list) -> None:
+        """fonction permettant d'ajouter la déviation actuelle associé au temps à une liste et de retirer les tuples obsolètes
+        entrée: un tuple 
+        sortie : une liste de tuple"""
         newTime = time()
         self.queue.append((newTime, value))
         while len(self.queue) > 0 and newTime - self.queue[0][0] >= self.delay:
             self.queue.pop(0)
 
     def moyenne(self, newValue: list,a=0,b=0) -> list:
+        """fonction permettant de faire la moyenne des deuxiemes valeurs de tuples
+        entrée: liste de tuple
+        sorite: liste de float"""
         self.updateQueue(newValue)
         for i in self.queue:
             a+=i[1][0]
@@ -37,6 +53,9 @@ class LinesProcess:
         return [a/len(self.queue),b/len(self.queue)]
 
     def linePosition(self, moy: list) -> int:
+        """fonction revoyant le signal à envoyer aux moteurs basée sur la moyenne
+        entrée: liste de float
+        sortie: un entier"""
         value=0
         for i in range(len(moy)):
             if moy[i]<0.33:
@@ -48,4 +67,7 @@ class LinesProcess:
         return value
 
     def output(self, lignes: list) -> int:
+        """fonction utilisant les fonctions ci-dessus
+        entrée: dict de listes
+        sortie: un entier"""
         return self.linePosition(self.moyenne(self.calcul(lignes)))
