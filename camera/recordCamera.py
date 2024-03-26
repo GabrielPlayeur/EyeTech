85d9d302ccc565d038b5b465b5fbabed79e7cc9e
Gabriel <teigne.gabriel@gmail.com>
2024-03-26 17:11:45 +0100

    Add record function to test camera

---
+++ camera/recordCamera.py
@@ -0,0 +1,18 @@
+from cameraSettings import Camera
+from time import time
+
+class Record:
+    def __init__(self) -> None:
+        self.cam = Camera()
+        self.cam.fps = 5
+    
+    def start(self, sec=30):
+        startTime = time()
+        while time()-startTime < sec:
+            frame = self.cam.capture_array()
+            self.cam.write(frame)
+        self.cam.shutdown()
+
+if __name__ == '__main__':
+    rec = Record()
+    rec.start()
