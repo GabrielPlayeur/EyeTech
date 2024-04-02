baf1c44d1e598ed2544e31e0b5f8ca602eba4df4
Gabriel <teigne.gabriel@gmail.com>
2024-04-02 16:27:02 +0200

    Modify gradian value

--- camera/test/video/video.analyse.py
+++ camera/test/video/video.analyse.py
@@ -5,13 +5,14 @@ sys.path.append(directory.parent.parent.parent)
 from detectLine import DetectLine
 import cv2
 
+size = (800,400)
 cap = cv2.VideoCapture(f"{sys.argv[1]}.mp4")
 while(cap.isOpened()):
     _, frame = cap.read()
     d = DetectLine(frame)
-    d.showImage(cv2.resize(d.cropped_canny, (300, 500)), "canny")
-    d.showImage(cv2.resize(d.lane_canny, (300, 500)), "lane canny")
-    d.showImage(cv2.resize(d.getFinalImage(), (300, 500)), "combo")
+    d.showImage(d.cropped_canny, "canny")
+    d.showImage(d.lane_canny, "lane canny")
+    d.showImage(d.getFinalImage(), "combo")
     print(d.lines, d.cropped_canny.shape)
     if cv2.waitKey(0) & 0xFF == ord('q'):
         break
